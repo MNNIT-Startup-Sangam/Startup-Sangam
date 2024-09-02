5914dc1e87acb6dfca781dc2924568c71456f89c
KrVatsal34 <kumarvatsal34@gmail.com>
2024-09-02 20:14:23 +0530

    basic structure of code

---
+++ src/components/Herosection.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Herosection = () => {
+  return (
+    <div>
+      <div className='pt-20 flex flex-col justify-center items-center'>
+  <div className='flex flex-col items-start'>
+    <div>MNNIT</div>
+    <h1 className='text-6xl text-[#d0fcb3]'>STARTUP</h1>
+    <h1 className='text-6xl text-[#d0fcb3]'>SANGAM</h1>
+  </div>
+</div>
+    </div>
+  )
+}
+
+export default Herosection
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>
+       <li className='p-6 pl-10 flex gap-16 text-white'>
+        <ul>Events</ul>
+        <ul>About Us</ul>
+        <ul>Contact</ul>
+       </li>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
