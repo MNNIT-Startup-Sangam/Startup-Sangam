6101033eff360a7b5e810a1bae3a08ca17414558
abhigit-saha <abhijitsaha1006@gmail.com>
2024-09-11 17:49:10 +0530

    fixed hydration error

--- src/components/ui/timeline.tsx
+++ src/components/ui/timeline.tsx
@@ -35,13 +35,11 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
 
   return (
     <div
-  className="w-full  dark:bg-[#08415c] font-sans md:px-10"
-  ref={containerRef}
->
-
+      className="w-full  dark:bg-[#08415c] font-sans md:px-10"
+      ref={containerRef}
+    >
       <div className="max-w-7xl mx-auto px-4 md:px-8 lg:px-10">
         {/* <div className="flex flex-col"> */}
-       
       </div>
 
       <div ref={ref} className="relative max-w-7xl mx-auto pb-20">
@@ -54,15 +52,15 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
               <div className="h-10 absolute left-3 md:left-3 w-10 rounded-full bg-white dark:bg-black flex items-center justify-center">
                 <div className="h-4 w-4 rounded-full bg-neutral-200 dark:bg-neutral-800 border border-neutral-300 dark:border-neutral-700 p-2" />
               </div>
-              <h3 className="hidden md:block text-xl md:pl-20 md:text-5xl font-bold text-neutral-500 dark:text-neutral-500 ">
+              <div className="hidden md:block text-xl md:pl-20 md:text-5xl font-bold text-neutral-500 dark:text-neutral-500 ">
                 {item.title}
-              </h3>
+              </div>
             </div>
 
             <div className="relative pl-20 pr-4 md:pl-4 w-full">
-              <h3 className="md:hidden block text-2xl mb-4 text-left font-bold text-neutral-500 dark:text-neutral-500">
+              <div className="md:hidden block text-2xl mb-4 text-left font-bold text-neutral-500 dark:text-neutral-500">
                 {item.title}
-              </h3>
+              </div>
               {item.content}{" "}
             </div>
           </div>
