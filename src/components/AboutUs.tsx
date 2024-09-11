b1253eb2080f7ed558ba055168fa3be089b90b25
abhigit-saha <abhijitsaha1006@gmail.com>
2024-09-11 19:01:50 +0530

    minor changes

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -6,7 +6,7 @@ export default function AboutUs() {
     <div className="relative w-full bg-black text-[#d0fcb3] p-6 flex flex-row">
       <div className="w-2/3 relative z-10">
         <h1 className="text-4xl font-bold mb-6">About Us</h1>
-        <p className="mb-4 text-white">
+        <p className="mb-4 text-white text-justify">
           The Innovation and Incubation Hub MNNIT Foundation, IIHMF at Motilal
           Nehru National Institute of Technology Allahabad is more than just a
           space, it is where ideas, research, and business plans come to life.
@@ -18,7 +18,7 @@ export default function AboutUs() {
           and access vital information, turning their ideas into reality.
         </p>
         <h1 className="text-4xl font-bold mb-6">About IIC</h1>
-        <p className="mb-4 text-white">
+        <p className="mb-4 text-white text-justify">
           The Ministry of Education (formerly the Ministry of Human Resource
           Development), Government of India, established the MHRD Innovation
           Cell to promote creativity and foster innovation within higher
@@ -32,7 +32,7 @@ export default function AboutUs() {
           framework.
         </p>
         <h1 className="text-4xl font-bold mb-6">Events Hosted by IIC</h1>
-        <div className="mb-4 text-white">
+        <div className="mb-4 text-white text-justify">
           MNNIT's IIC organizes a variety of events to promote innovation and
           entrepreneurship among students, such as:
           <ul className="list-disc ml-5">
@@ -51,7 +51,7 @@ export default function AboutUs() {
       </div>
 
       {/* Interactive Globe on the right side */}
-      <div className="w-1/3 flex items-center justify-center absolute top-0 right-0 h-full z-0 overflow-hidden">
+      <div className="w-1/3 flex items-center justify-center absolute top-0 right-0 h-full z-0">
         <GlobeDemo />
       </div>
     </div>
